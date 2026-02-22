4753962c1970475118d1224fbd7dc0ad9607a637
Micro-ATP <131521928+Micro-ATP@users.noreply.github.com>
2026-02-22 21:58:13 +0800

    修复已知问题

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -15,6 +15,9 @@ pub use error::{AppError, Result};
 
 fn main() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_shell::init())
+        .plugin(tauri_plugin_fs::init())
         .invoke_handler(tauri::generate_handler![
             commands::disk::list_disks,
             commands::disk::get_disk_info,
